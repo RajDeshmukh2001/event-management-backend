d79f1bad47d38dc93ced6d5ab7ee7e209c5f8cbd
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-03-06 09:03:34 +0530

    feat: add UserRepository

---
+++ src/main/java/com/project/event_management_system/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.project.event_management_system.repository;
+
+import com.project.event_management_system.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
